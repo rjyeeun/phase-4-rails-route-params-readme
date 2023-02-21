b23ed1b9be412fe3a136b440da1b4d47e06257d7
Yeeun Jung <rjyeeun@gmail.com>
2023-02-21 15:59:51 -0500

    commit tree 3c23b0ad3b99e7bb522d1e68dea0e0dfe0761e09

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,9 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
-
+  
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+  end
 end
